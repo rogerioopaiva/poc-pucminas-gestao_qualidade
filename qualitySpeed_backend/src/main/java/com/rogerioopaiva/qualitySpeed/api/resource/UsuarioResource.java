f0f668bc7b24cd49a0100ed0b0398bbdb333e82d
Rogério Paiva <rogerioopaiva36@gmail.com>
2020-06-04 20:57:46 -0300

    Criacao api usuarios e inicio da colaboradores

---
+++ qualitySpeed_backend/src/main/java/com/rogerioopaiva/qualitySpeed/api/resource/UsuarioResource.java
@@ -0,0 +1,49 @@
+package com.rogerioopaiva.qualitySpeed.api.resource;
+
+import com.rogerioopaiva.qualitySpeed.api.dto.UsuarioDTO;
+import com.rogerioopaiva.qualitySpeed.exception.RegraNegocioException;
+import com.rogerioopaiva.qualitySpeed.model.entity.Usuario;
+import com.rogerioopaiva.qualitySpeed.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+        private UsuarioService service;
+
+        public UsuarioResource( UsuarioService service ) {
+            this.service = service;
+        }
+
+        @PostMapping("/autenticar")
+        public ResponseEntity autenticar( @RequestBody UsuarioDTO dto) {
+            try {
+                Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+                return ResponseEntity.ok(usuarioAutenticado);
+            }catch (RegraNegocioException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
+
+        @PostMapping
+        public ResponseEntity salvar( @RequestBody UsuarioDTO dto ) {
+
+            Usuario usuario = Usuario.builder()
+                    .nome(dto.getNome())
+                    .email(dto.getEmail())
+                    .senha(dto.getSenha()).build();
+
+            try {
+               Usuario usuarioSalvo = service.salvarUsuario(usuario);
+               return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+            }catch (RegraNegocioException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
+    }
---
+++ qualitySpeed_backend/src/main/java/com/rogerioopaiva/qualitySpeed/service/UsuarioService.java
@@ -0,0 +1,12 @@
+package com.rogerioopaiva.qualitySpeed.service;
+
+import com.rogerioopaiva.qualitySpeed.model.entity.Usuario;
+
+public interface UsuarioService {
+
+    Usuario autenticar(String email, String senha);
+
+    Usuario salvarUsuario(Usuario usuario);
+
+    void validarEmail(String email);
+}
