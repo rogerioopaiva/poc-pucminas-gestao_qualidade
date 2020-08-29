dda18de5beded4f10cfb5def4afcb544a4f9636d
Rogério Paiva <rogerioopaiva36@gmail.com>
2020-08-29 15:31:48 -0300

    adicionando a url da api remota

--- qualityspeed-frontend/src/app/apiservice.js
+++ qualityspeed-frontend/src/app/apiservice.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
-    //baseURL: 'https://qualityspeed-api.herokuapp.com/'
-})
+  //baseURL: "http://localhost:8080",
+  baseURL: "https://qualityspeed-backend.herokuapp.com/",
+});
 
 class ApiService {
 
